52806c5881bbeab7018d75531677f8820dc77b22
LWanjiru <linnette.wanjiru@andela.com>
2017-07-17 18:37:31 +0300

    [feature #148963113] configure Users.js,create end points and tests

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,8 +1,14 @@
 const RoleController = require('../controllers/Roles');
+const UserController = require('../controllers/Users');
+
+
 require('../../app');
 
 module.exports = (app) => {
-  app.post('/api/roles/add', RoleController.create);
+  app.post('/api/roles', RoleController.create);
   app.get('/api/roles', RoleController.list);
   app.delete('/api/roles', RoleController.deleteAll);
+
+  app.post('/users', UserController.create);
+  app.get('/users', UserController.list);
 };
