afc2c150c5a334d1b966fb41c15f2c1123ae94ff
LWanjiru <linnette.wanjiru@andela.com>
2017-07-18 00:18:06 +0300

    [feature #148963113] remove cached coverage folder, add tests, controller methods and routes, fix travis for testing

--- server/routes/route.js
+++ server/routes/route.js
@@ -2,13 +2,14 @@ const RoleController = require('../controllers/Roles');
 const UserController = require('../controllers/Users');
 
 
-require('../../app');
+require('./../../App');
 
 module.exports = (app) => {
   app.post('/api/roles', RoleController.create);
   app.get('/api/roles', RoleController.list);
   app.delete('/api/roles', RoleController.deleteAll);
 
-  app.post('/users', UserController.create);
-  app.get('/users', UserController.list);
+  app.post('/users/', UserController.create);
+  app.get('/users/', UserController.list);
+  app.delete('/users/', UserController.deleteAll);
 };
