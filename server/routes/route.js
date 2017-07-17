e7311a97ad199c7c05e343d9e5b471dd0967ade7
LWanjiru <linnette.wanjiru@andela.com>
2017-07-17 23:38:18 +0200

    [feature #148963113] modify .travis.yml and package.json

--- server/routes/route.js
+++ server/routes/route.js
@@ -2,7 +2,7 @@ const RoleController = require('../controllers/Roles');
 const UserController = require('../controllers/Users');
 
 
-require('./../../App');
+require('./../../app');
 
 module.exports = (app) => {
   app.post('/api/roles', RoleController.create);
