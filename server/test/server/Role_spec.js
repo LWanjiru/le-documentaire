c6c56b62f6c590cef9e22d4a2eee6fb0907ee6bd
LWanjiru <linnette.wanjiru@andela.com>
2017-07-28 14:42:09 +0200

    Modify controllers/Roles.js and refactor Role_spec.js for testing

--- server/test/server/Role_spec.js
+++ server/test/server/Role_spec.js
@@ -1,40 +1,70 @@
 /* global describe it beforeEach*/
+const chai = require('chai');
 const app = require('./../../../App');
 const request = require('supertest');
+
+const expect = chai.expect;
 
 describe('POST /api/roles ', () => {
   beforeEach((done) => {
     request(app).delete('/api/roles').end(done);
   });
 
-  it('responds with a 201 the created role on success', (done) => {
+  it('POST /api/roles/add should respond with a 201 the created role on success', (done) => {
     request(app)
-      .post('/api/roles')
+      .post('/api/roles/add')
+      .set('Accept', 'application/x-www-form-urlencoded')
       .send({ title: 'author', description: 'They write' })
-      .set('Accept', 'application/json')
-      .expect(201)
-      .expect('Content-Type', /json/)
-      .expect(/"title":\s*"author"/)
-      .expect(/"description":\s*"They write"/, done);
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(201);
+        expect(res.body.newRole.title).to.equal('author');
+        expect(res.body.newRole.description).to.equal('They write');
+        done();
+      });
   });
 });
-describe('POST /api/roles ', () => {
-  beforeEach((done) => {
-    request(app).post('/api/roles').end(done);
-  });
-  it('responds with a 400 the role not created for duplicate', (done) => {
-    request(app)
-  .post('/api/roles')
-  .send({ title: 'author', description: 'They write' })
-  .set('Accept', 'application/json')
-  .expect(400)
-  .expect('Content-Type', /json/, done);
-  });
-});
-it('GET /roles responds with a 200 on success', (done) => {
+
+it('POST /api/roles/add responds with a 409 the role not created for duplicate', (done) => {
   request(app)
-  .get('/api/roles')
-  .set('Accept', 'application/json')
-  .expect(200)
-  .expect('Content-Type', /json/, done);
+    .post('/api/roles/add')
+    .send({ title: 'author', description: 'They write' })
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      expect(res.statusCode).to.equal(409);
+      expect(res.body).to.include({ message: 'Role already exists!' });
+      done();
+    });
 });
+
+it('PUT /roles responds with 400 if field is empty', (done) => {
+  request(app)
+  .put('/api/roles')
+  .send({ title: '', description: 'they dance' })
+  .send({ title: 'something', description: '' })
+  .end((err, res) => {
+    expect(res.statusCode).to.equal(400);
+    expect(res.body).to.include({ message: 'All fields are required!' });
+    done();
+  });
+});
+
+describe('GET /api/roles ', () => {
+  beforeEach((done) => {
+    request(app).post('/api/roles/add').end(done);
+  });
+
+  it('GET /api/roles responds with a 200 on success', (done) => {
+    request(app)
+    .get('/api/roles')
+    .set('Accept', 'application/json')
+    .end((err, res) => {
+      expect(res.statusCode).to.equal(200);
+      expect(res.body).to.be.an('array');
+      expect(res.body[0]).to.have.property('title');
+      expect(res.body[0]).to.have.property('description');
+      done();
+    });
+  });
+});
+
