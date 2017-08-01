6cc69d2a7cab5d9262c32f32f1fece92c6a0b14c
LWanjiru <linnette.wanjiru@andela.com>
2017-08-01 11:08:04 +0300

    [feature #148963113]refine roles controllers and validation. Append user create method and model for validation

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -1,17 +1,53 @@
 const { User } = require('../models');
+const passwordHash = require('password-hash');
 
 module.exports = {
+
   create(req, res) {
-    User.create({
-      username: req.body.username,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password,
+    // console.log(User);
+    User.findOne({
+      where: {
+        username: req.body.username,
+      },
     })
-    .then(user => res.status(201).send(user))
-    .catch(error => res.status(400).send(error));
+    .then((user) => {
+      if (user) {
+        res.send({ message: 'This user already exists!' });
+      } else if (
+          !req.body.username || !req.body.firstName ||
+          !req.body.lastName || !req.body.password || !req.body.email) {
+        res.status(400).send({ message: 'All fields are required!' });
+      } else if (user === null) {
+        User.findOne({
+          where: {
+            email: req.body.email,
+          },
+        })
+        .then((email) => {
+          const expression = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+          if (email) {
+            res.send({ message: 'Email is registered to a different username.' });
+          } else if ((req.body.password).length < 6) {
+            res.send({ message: 'Password must be between 6 and 20 characters' });
+          } else if (expression.test(req.body.email)) {
+            User.create({
+              username: req.body.username,
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              email: req.body.email,
+              password: passwordHash.generate(req.body.password),
+            })
+        .then((newUser => res.status(201).send({ newUser, message: 'User created successfully!' })));
+          } else {
+            res.send({ message: 'Enter valid email format' });
+          }
+        });
+      }
+      // }
+    });
   },
+
   list(req, res) {
     User.findAll()
     .then(role => res.status(200).send(role))
--- server/routes/Route.js
+++ server/routes/Route.js
@@ -8,12 +8,11 @@ module.exports = (app) => {
   app.post('/api/roles/add', RoleController.create);
   app.get('/api/roles', RoleController.listAll);
   app.get('/api/roles/:title', RoleController.listOne);
+  app.put('/api/roles', RoleController.update);
   app.delete('/api/roles', RoleController.deleteAll);
   app.delete('/api/roles/:title', RoleController.deleteOne);
 
-  app.put('/api/roles', RoleController.update);
-
-  app.post('/users/', UserController.create);
-  app.get('/users/', UserController.list);
-  app.delete('/users/', UserController.deleteAll);
+  app.post('/users', UserController.create);
+  app.get('/users', UserController.list);
+  app.delete('/users', UserController.deleteAll);
 };
