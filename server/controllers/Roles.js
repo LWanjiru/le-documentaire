8299fef07a5b6cc1e6c97d22f9255a5c967d9b9c
linnettewanjiru <linnette.wanjiru@andela.com>
2019-05-22 06:25:55 +0300

    fix merge conflict

--- server/controllers/Roles.js
+++ server/controllers/Roles.js
@@ -107,5 +107,12 @@ module.exports = {
         });
     }
   },
+  deleteAll(req, res) {
+    if (process.env.NODE_ENV === 'test') {
+      Role.truncate({ cascade: true, restartIdentity: true }).then(() => res.status(204).send({}));
+    } else {
+      res.status(403).send({ message: 'That action is not allowed!' });
+    }
+  },
 };
 
---
+++ server/routes/route.js
@@ -0,0 +1,90 @@
+const RoleController = require('../controllers/Roles');
+const UserController = require('../controllers/Users');
+const DocumentController = require('../controllers/Documents');
+
+
+require('../App');
+
+module.exports = (app) => {
+  /**
+   * ROLE routes
+   */
+  // Add a role
+  app.post('/roles/add', UserController.authenticate, UserController.admin, RoleController.create);
+
+  // List, Delete ALL roles
+  app.route('/roles')
+  .get(UserController.authenticate, UserController.admin, RoleController.listAll)
+  .delete(UserController.authenticate, UserController.admin, RoleController.deleteAll);
+
+  // View, Update, Delete ONE role by title
+  app.route('/roles/:title')
+  .get(UserController.authenticate, UserController.admin, RoleController.listOne)
+  .put(UserController.authenticate, UserController.admin, RoleController.update)
+  .delete(UserController.authenticate, UserController.admin, RoleController.deleteOne);
+
+  /**
+   * USER routes
+   */
+  // Login a user
+  app.post('/users/login', UserController.login);
+
+  // Sign up a new user
+  app.post('/users/', UserController.create);
+
+  // Logout user
+  app.post('/users/logout', UserController.authenticate, UserController.logout);
+
+  // Paginate and List all users
+  app.route('/users')
+  .get(UserController.authenticate, UserController.paginateUsers)
+   // Delete ALL users
+  .delete(UserController.authenticate, UserController.admin, UserController.deleteAll);
+
+    // List All users without pagination
+  app.get('/users/all', UserController.authenticate, UserController.listAll);
+
+  // List, Update, Delete one user by ID
+  app.route('/users/:id')
+  .get(UserController.authenticate, UserController.listOne)
+  .put(UserController.authenticate, UserController.update)
+  .delete(UserController.authenticate, UserController.admin, UserController.deleteOne);
+
+
+  // Search for a user
+  app.get('/search/users/', UserController.userSearch);
+
+ /**
+  * DOCUMENT routes
+  */
+  // Create a document
+  app.post('/documents', UserController.authenticate, DocumentController.create);
+
+  // View ALL user documents regardless of access(Admin)
+  app.get('/documents/all', UserController.authenticate, UserController.admin, DocumentController.listAll);
+
+  // View All Public documents
+  app.get('/documents/public', DocumentController.listPublic);
+
+  // View all of a user's documents
+  app.get('/users/:id/documents', UserController.authenticate, DocumentController.listUserDocs);
+
+  // Get or Delete a document by ID
+  app.route('/documents/:id')
+  .get(UserController.authenticate, DocumentController.listOneDoc)
+  .put(UserController.authenticate, DocumentController.update);
+
+
+  // View all documents whose access is allowed to only users
+  //  with the logged in user's role
+  app.get('/documents/users/role', UserController.authenticate, DocumentController.listDocsByRole);
+
+  //  Delete a document by ID
+  app.delete('/documents/:id', UserController.authenticate, DocumentController.deleteOne);
+
+  // Paginate and List all documents
+  app.get('/documents/', UserController.authenticate, DocumentController.paginateDocs);
+
+  // Search for a document by TITLE
+  app.get('/search/documents/', DocumentController.documentSearch);
+};
