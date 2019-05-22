f1d92eb27b1caeb51ad6241160a4176506a85c14
linnettewanjiru <linnette.wanjiru@andela.com>
2019-05-22 07:20:08 +0300

    fix merge conflict

--- server/controllers/Users.js
+++ server/controllers/Users.js
@@ -288,7 +288,7 @@ module.exports = {
     } else {
       User.findById(req.params.id)
         .then((user) => {
-          if (!user) {
+          if (!user || user.length < 1) {
             res.status(404).send({ message: 'User doesn\'t exist' });
           } else {
             User.destroy({
