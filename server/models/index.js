d2afb6bbaa25a64833f5ecfeb23b4a2f686d6856
linnettewanjiru <linnette.wanjiru@andela.com>
2019-05-24 09:31:16 +0300

    fix styling issue

--- server/models/index.js
+++ server/models/index.js
@@ -32,7 +32,8 @@ if (config.environment === 'production') {
 fs
   .readdirSync(__dirname)
   .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => { const model = sequelize.import(path.join(__dirname, file));
+  .forEach((file) => {
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
